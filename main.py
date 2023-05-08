fe92d4b35972222c123d0a2a285f680e63160831
Tirth Nagar <tirthnagar.n1@gmail.com>
2023-05-07 22:42:54 -0400

    add manga/manhwa/manhua to reading list

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+def getUrls(titles):
+    baseUrl = "http://fanfox.net/manga/"
+    formatTitle = lambda title : title.casefold().replace(" ", "_").replace(":", "").replace("!", "").replace("?", "").replace("'", "_").replace("(","").replace(")","").replace(",","").replace(".","").replace("-","_")
+    return [baseUrl+formatTitle(title) for title in titles]
+
 load_dotenv()
 
 DISCORD_TOKEN  = os.getenv('DISCORD_TOKEN')
@@ -13,9 +18,9 @@ CONNECTION_URL = os.getenv('CONNECTION_URL')
 intents = discord.Intents.all()
 
 cluster = MongoClient(CONNECTION_URL)
-database = cluster["User-Data"]
+database = cluster["Manga-Update"]
 collection = database["User-Data"]
-print("Database connected")
+# print(database)
 
 # Setting up the bot
 bot = commands.Bot(command_prefix='?',intents=intents,pass_context=True)
@@ -24,15 +29,39 @@ bot = commands.Bot(command_prefix='?',intents=intents,pass_context=True)
 @bot.command(name='list_add')
 async def list_add(ctx):
     
-    reading_list = ctx.message.content.strip('?list_add ')
-    reading_list = reading_list.split('/')
-    print(reading_list)
+    query = { "_id": str(ctx.message.author)}
     
-    post = {"_id": ctx.message.author, "Reading_List": reading_list}
+    if collection.count_documents(query) == 0:
     
+        reading_list = str(ctx.message.content).strip('?list_add ')
+        reading_list = reading_list.split('/')
+        
+        url_list = getUrls(reading_list)
+                
+        post = {"_id": str(ctx.message.author), "Reading_List": reading_list, "Url_List": url_list}
+                
+        collection.insert_one(post)
+        await ctx.channel.send("The Specified Manga have been added to your reading list!")
     
-    print(str(ctx.message.author))
-    print(str(ctx.message.channel))
-    print(str(ctx.message.content))
+    else:
+        user_data = collection.find(query)
+        
+        for data in user_data:
+            reading_list = data["Reading_List"]
+            url_list = data["Url_List"]      
+           
+        extend_reading = str(ctx.message.content).strip('?list_add ')
+        extend_reading = extend_reading.split('/')
+        
+        extend_url = getUrls(extend_reading)
+        
+        reading_list.extend(extend_reading)
+        url_list.extend(extend_url)
+        
+        collection.update_many({"_id": str(ctx.message.author)}, {"$set": {"Reading_List": reading_list,"Url_List": url_list}})
+        await ctx.channel.send('The Specified Manga have been added to your reading list!')
+
+
+
 
 bot.run(DISCORD_TOKEN)
